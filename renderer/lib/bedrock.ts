c1aa498694c07827eb2467db8984a903e8a30d75
Jess Szmajda <szmajdj@amazon.com>
2025-02-26 09:48:38 -0500

    Added Claude 3.7 Sonnet

--- renderer/lib/bedrock.ts
+++ renderer/lib/bedrock.ts
@@ -32,6 +32,14 @@ export const getAvailableModels = async (): Promise<Model[]> => {
             costPerOutputTokenK: 0.015,
             region: 'us-west-2',
             maxContextTokens: 200000
+        },
+        {
+            name: "Claude 3.7 Sonnet",
+            modelId: "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+            costPerInputTokenK: 0.003,
+            costPerOutputTokenK: 0.015,
+            region: 'us-east-1',
+            maxContextTokens: 200000
         }
     ];
 };
